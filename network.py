6f17050f18a4979ae23157ab430751d919b14cdb
Hyeonwoo Kang <znxlwm@gmail.com>
2017-08-10 16:04:44 +0900

    Add files via upload

---
+++ network.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class generator(nn.Module):
+    # initializers
+    def __init__(self, d=64):
+        super(generator, self).__init__()
+        # Unet encoder
+        self.conv1 = nn.Conv2d(3, d, 4, 2, 1)
+        self.conv2 = nn.Conv2d(d, d * 2, 4, 2, 1)
+        self.conv2_bn = nn.BatchNorm2d(d * 2)
+        self.conv3 = nn.Conv2d(d * 2, d * 4, 4, 2, 1)
+        self.conv3_bn = nn.BatchNorm2d(d * 4)
+        self.conv4 = nn.Conv2d(d * 4, d * 8, 4, 2, 1)
+        self.conv4_bn = nn.BatchNorm2d(d * 8)
+        self.conv5 = nn.Conv2d(d * 8, d * 8, 4, 2, 1)
+        self.conv5_bn = nn.BatchNorm2d(d * 8)
+        self.conv6 = nn.Conv2d(d * 8, d * 8, 4, 2, 1)
+        self.conv6_bn = nn.BatchNorm2d(d * 8)
+        self.conv7 = nn.Conv2d(d * 8, d * 8, 4, 2, 1)
+        self.conv7_bn = nn.BatchNorm2d(d * 8)
+        self.conv8 = nn.Conv2d(d * 8, d * 8, 4, 2, 1)
+        # self.conv8_bn = nn.BatchNorm2d(d * 8)
+
+        # Unet decoder
+        self.deconv1 = nn.ConvTranspose2d(d * 8, d * 8, 4, 2, 1)
+        self.deconv1_bn = nn.BatchNorm2d(d * 8)
+        self.deconv2 = nn.ConvTranspose2d(d * 8 * 2, d * 8, 4, 2, 1)
+        self.deconv2_bn = nn.BatchNorm2d(d * 8)
+        self.deconv3 = nn.ConvTranspose2d(d * 8 * 2, d * 8, 4, 2, 1)
+        self.deconv3_bn = nn.BatchNorm2d(d * 8)
+        self.deconv4 = nn.ConvTranspose2d(d * 8 * 2, d * 8, 4, 2, 1)
+        self.deconv4_bn = nn.BatchNorm2d(d * 8)
+        self.deconv5 = nn.ConvTranspose2d(d * 8 * 2, d * 4, 4, 2, 1)
+        self.deconv5_bn = nn.BatchNorm2d(d * 4)
+        self.deconv6 = nn.ConvTranspose2d(d * 4 * 2, d * 2, 4, 2, 1)
+        self.deconv6_bn = nn.BatchNorm2d(d * 2)
+        self.deconv7 = nn.ConvTranspose2d(d * 2 * 2, d, 4, 2, 1)
+        self.deconv7_bn = nn.BatchNorm2d(d)
+        self.deconv8 = nn.ConvTranspose2d(d * 2, 3, 4, 2, 1)
+
+    # weight_init
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    # forward method
+    def forward(self, input):
+        e1 = self.conv1(input)
+        e2 = self.conv2_bn(self.conv2(F.leaky_relu(e1, 0.2)))
+        e3 = self.conv3_bn(self.conv3(F.leaky_relu(e2, 0.2)))
+        e4 = self.conv4_bn(self.conv4(F.leaky_relu(e3, 0.2)))
+        e5 = self.conv5_bn(self.conv5(F.leaky_relu(e4, 0.2)))
+        e6 = self.conv6_bn(self.conv6(F.leaky_relu(e5, 0.2)))
+        e7 = self.conv7_bn(self.conv7(F.leaky_relu(e6, 0.2)))
+        e8 = self.conv8(F.leaky_relu(e7, 0.2))
+        # e8 = self.conv8_bn(self.conv8(F.leaky_relu(e7, 0.2)))
+        d1 = F.dropout(self.deconv1_bn(self.deconv1(F.relu(e8))), 0.5, training=True)
+        d1 = torch.cat([d1, e7], 1)
+        d2 = F.dropout(self.deconv2_bn(self.deconv2(F.relu(d1))), 0.5, training=True)
+        d2 = torch.cat([d2, e6], 1)
+        d3 = F.dropout(self.deconv3_bn(self.deconv3(F.relu(d2))), 0.5, training=True)
+        d3 = torch.cat([d3, e5], 1)
+        d4 = self.deconv4_bn(self.deconv4(F.relu(d3)))
+        # d4 = F.dropout(self.deconv4_bn(self.deconv4(F.relu(d3))), 0.5)
+        d4 = torch.cat([d4, e4], 1)
+        d5 = self.deconv5_bn(self.deconv5(F.relu(d4)))
+        d5 = torch.cat([d5, e3], 1)
+        d6 = self.deconv6_bn(self.deconv6(F.relu(d5)))
+        d6 = torch.cat([d6, e2], 1)
+        d7 = self.deconv7_bn(self.deconv7(F.relu(d6)))
+        d7 = torch.cat([d7, e1], 1)
+        d8 = self.deconv8(F.relu(d7))
+        o = F.tanh(d8)
+
+        return o
+
+class discriminator(nn.Module):
+    # initializers
+    def __init__(self, d=64):
+        super(discriminator, self).__init__()
+        self.conv1 = nn.Conv2d(6, d, 4, 2, 1)
+        self.conv2 = nn.Conv2d(d, d * 2, 4, 2, 1)
+        self.conv2_bn = nn.BatchNorm2d(d * 2)
+        self.conv3 = nn.Conv2d(d * 2, d * 4, 4, 2, 1)
+        self.conv3_bn = nn.BatchNorm2d(d * 4)
+        self.conv4 = nn.Conv2d(d * 4, d * 8, 4, 1, 1)
+        self.conv4_bn = nn.BatchNorm2d(d * 8)
+        self.conv5 = nn.Conv2d(d * 8, 1, 4, 1, 1)
+
+    # weight_init
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    # forward method
+    def forward(self, input, label):
+        x = torch.cat([input, label], 1)
+        x = F.leaky_relu(self.conv1(x), 0.2)
+        x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
+        x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
+        x = F.leaky_relu(self.conv4_bn(self.conv4(x)), 0.2)
+        x = F.sigmoid(self.conv5(x))
+
+        return x
+
+def normal_init(m, mean, std):
+    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+        m.weight.data.normal_(mean, std)
+        m.bias.data.zero_()
---
+++ util.py
@@ -0,0 +1,117 @@
+import itertools, imageio, torch
+import matplotlib.pyplot as plt
+import numpy as np
+from torchvision import datasets
+from scipy.misc import imresize
+
+def show_result(G, x_, y_, num_epoch, show = False, save = False, path = 'result.png'):
+    # G.eval()
+    test_images = G(x_)
+
+    size_figure_grid = 3
+    fig, ax = plt.subplots(x_.size()[0], size_figure_grid, figsize=(5, 5))
+    for i, j in itertools.product(range(x_.size()[0]), range(size_figure_grid)):
+        ax[i, j].get_xaxis().set_visible(False)
+        ax[i, j].get_yaxis().set_visible(False)
+
+    for i in range(x_.size()[0]):
+        ax[i, 0].cla()
+        ax[i, 0].imshow((x_[i].cpu().data.numpy().transpose(1, 2, 0) + 1) / 2)
+        ax[i, 1].cla()
+        ax[i, 1].imshow((test_images[i].cpu().data.numpy().transpose(1, 2, 0) + 1) / 2)
+        ax[i, 2].cla()
+        ax[i, 2].imshow((y_[i].numpy().transpose(1, 2, 0) + 1) / 2)
+
+    label = 'Epoch {0}'.format(num_epoch)
+    fig.text(0.5, 0.04, label, ha='center')
+
+    if save:
+        plt.savefig(path)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
+    x = range(len(hist['D_losses']))
+
+    y1 = hist['D_losses']
+    y2 = hist['G_losses']
+
+    plt.plot(x, y1, label='D_loss')
+    plt.plot(x, y2, label='G_loss')
+
+    plt.xlabel('Iter')
+    plt.ylabel('Loss')
+
+    plt.legend(loc=4)
+    plt.grid(True)
+    plt.tight_layout()
+
+    if save:
+        plt.savefig(path)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+def generate_animation(root, model, opt):
+    images = []
+    for e in range(opt.train_epoch):
+        img_name = root + 'Fixed_results/' + model + str(e + 1) + '.png'
+        images.append(imageio.imread(img_name))
+    imageio.mimsave(root + model + 'generate_animation.gif', images, fps=5)
+
+def data_load(path, subfolder, transform, opt):
+    dset = datasets.ImageFolder(path, transform)
+    ind = dset.class_to_idx[subfolder]
+
+    n = 0
+    for i in range(dset.__len__()):
+        if ind != dset.imgs[n][1]:
+            del dset.imgs[n]
+            n -= 1
+
+        n += 1
+
+    return torch.utils.data.DataLoader(dset, batch_size=opt.batch_size, shuffle=True)
+
+def imgs_resize(imgs, resize_scale = 286):
+    outputs = torch.FloatTensor(imgs.size()[0], imgs.size()[1], resize_scale, resize_scale)
+    for i in range(imgs.size()[0]):
+        img = imresize(imgs[i].numpy(), [resize_scale, resize_scale])
+        outputs[i] = torch.FloatTensor((img.transpose(2, 0, 1).astype(np.float32).reshape(-1, imgs.size()[1], resize_scale, resize_scale) - 127.5) / 127.5)
+
+    return outputs
+
+def random_crop(imgs1, imgs2, crop_size = 256):
+    outputs1 = torch.FloatTensor(imgs1.size()[0], imgs1.size()[1], crop_size, crop_size)
+    outputs2 = torch.FloatTensor(imgs2.size()[0], imgs2.size()[1], crop_size, crop_size)
+    for i in range(imgs1.size()[0]):
+        img1 = imgs1[i]
+        img2 = imgs2[i]
+        rand1 = np.random.randint(0, imgs1.size()[2] - crop_size)
+        rand2 = np.random.randint(0, imgs2.size()[2] - crop_size)
+        outputs1[i] = img1[:, rand1: crop_size + rand1, rand2: crop_size + rand2]
+        outputs2[i] = img2[:, rand1: crop_size + rand1, rand2: crop_size + rand2]
+
+    return outputs1, outputs2
+
+def random_fliplr(imgs1, imgs2):
+    outputs1 = torch.FloatTensor(imgs1.size())
+    outputs2 = torch.FloatTensor(imgs2.size())
+    for i in range(imgs1.size()[0]):
+        if torch.rand(1)[0] < 0.5:
+            img1 = torch.FloatTensor(
+                (np.fliplr(imgs1[i].numpy().transpose(1, 2, 0)).transpose(2, 0, 1).reshape(-1, imgs1.size()[1], imgs1.size()[2], imgs1.size()[3]) + 1) / 2)
+            outputs1[i] = (img1 - 0.5) / 0.5
+            img2 = torch.FloatTensor(
+                (np.fliplr(imgs2[i].numpy().transpose(1, 2, 0)).transpose(2, 0, 1).reshape(-1, imgs2.size()[1], imgs2.size()[2], imgs2.size()[3]) + 1) / 2)
+            outputs2[i] = (img2 - 0.5) / 0.5
+        else:
+            outputs1[i] = imgs1[i]
+            outputs2[i] = imgs2[i]
+
+    return outputs1, outputs2
